48e6366b126d7728353e6751bd8c6c1f83e836c5
pratikpalav03 <71515230+pratikpalav03@users.noreply.github.com>
2021-08-02 15:07:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ loadAllTimeChart = async(country) => {
 
     } else{
         console.log("pratik",country);
-        let confirmed =  await covidApi.getCountryAllTimeCases(country,CASE_STATUS.confirmed)
+        let confirmed =  await covidApi.renderWorldData(country,CASE_STATUS.confirmed)
         let recovered =  await covidApi.getCountryAllTimeCases(country, CASE_STATUS.recovered)
         let deaths =  await covidApi.getCountryAllTimeCases(country, CASE_STATUS.deaths)
 
